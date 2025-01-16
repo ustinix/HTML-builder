d4665aadaa187f9b2be0056afed3e95a5674aaa0
Ustinix <ksuletters@yandex.ru>
2025-01-16 13:55:51 +0300

    feat: 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+const pathTxt = path.join(__dirname, 'text.txt');
+console.log(pathTxt);
+
+const readStream = fs.createReadStream(pathTxt);
+
+readStream.on('data', (chunk) => {
+    console.log(chunk.toString());
+})
